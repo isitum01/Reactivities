a0964307e9ccd476d9360131f4fa7da95b6b4649
Ivan Šitum <isitum01@gmail.com>
2024-02-08 21:03:01 +0100

    Delete Activity Handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,7 +18,7 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")] //api/activities/5
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return await Mediator.Send(new Details.Query{ Id = id });
+        return await Mediator.Send(new Details.Query { Id = id });
     }
 
     [HttpPost]
@@ -39,4 +39,11 @@ public class ActivitiesController : BaseApiController
         return Ok();
     }
 
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+        await Mediator.Send(new Delete.Command { Id = id });
+
+        return Ok();
+    }
 }
