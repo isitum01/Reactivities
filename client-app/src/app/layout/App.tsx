251f3793dd0def4e0819412624c115add89f5cff
Ivan Šitum <isitum01@gmail.com>
2024-02-09 11:58:31 +0100

    Selecting an activity to view, and canceling it

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -15,11 +16,24 @@ function App() {
       })
   }, [])
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(activity => activity.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+        />
       </Container>
     </>
   )
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -2,10 +2,11 @@ import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Image
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
-  activity: Activity
+  activity: Activity;
+  cancelSelectActivity: () => void;
 }
 
-export default function ActivityDetails({ activity }: Props) {
+export default function ActivityDetails({ activity, cancelSelectActivity }: Props) {
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
@@ -21,7 +22,7 @@ export default function ActivityDetails({ activity }: Props) {
       <CardContent extra>
         <Button.Group widths="2">
           <Button basic color="blue" content="Edit"/>
-          <Button basic color="grey" content="Cancel"/>
+          <Button onClick={cancelSelectActivity} basic color="grey" content="Cancel"/>
         </Button.Group>
       </CardContent>
     </Card>
