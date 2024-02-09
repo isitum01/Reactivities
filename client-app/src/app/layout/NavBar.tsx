de6a561579794b302888d6cb249a827fbe4080d4
Ivan Šitum <isitum01@gmail.com>
2024-02-09 09:50:04 +0100

    Added navbar styles

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
 
 export default function NavBar() {
@@ -6,7 +5,7 @@ export default function NavBar() {
     <Menu inverted fixed="top">
       <Container>
         <Menu.Item header>
-          <img src="/assets/logo.png" alt="logo" />
+          <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
           Reactivities
         </Menu.Item>
         <Menu.Item name="Activities" />
