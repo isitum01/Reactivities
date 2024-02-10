240e606f785c7dccd2f6097a758689c9f44a033b
Ivan Šitum <isitum01@gmail.com>
2024-02-10 23:04:07 +0100

    Using JS Map object to store activities

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,7 +5,7 @@ import { observer } from "mobx-react-lite";
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
-  const { deleteActivity, activities, loading } = activityStore;
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
 
   const [target, setTarget] = useState('');
 
@@ -17,31 +17,29 @@ export default observer(function ActivityList() {
   return (
     <Segment>
       <Item.Group divided>
-        {
-          activities.map(activity => (
-            <Item key={activity.id}>
-              <Item.Content>
-                <Item.Header as='a'>{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                  <div>{activity.description}</div>
-                  <div>{activity.city}, {activity.venue}</div>
-                </Item.Description>
-                <Item.Extra>
-                  <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content="View" color="blue" />
-                  <Button
-                    name={activity.id}
-                    loading={loading && target === activity.id}
-                    onClick={(e) => handleActivityDelete(e, activity.id)}
-                    floated="right"
-                    content="Delete" color="red"
-                  />
-                  <Label basic content={activity.category} />
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          ))
-        }
+        {activitiesByDate.map(activity => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as='a'>{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>{activity.city}, {activity.venue}</div>
+              </Item.Description>
+              <Item.Extra>
+                <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                <Button
+                  name={activity.id}
+                  loading={loading && target === activity.id}
+                  onClick={(e) => handleActivityDelete(e, activity.id)}
+                  floated="right"
+                  content="Delete" color="red"
+                />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
       </Item.Group>
     </Segment>
   )
