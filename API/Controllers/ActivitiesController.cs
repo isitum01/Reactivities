e05980ce13a6cf6bd7855f356a6d2b58c834ecb0
Ivan Šitum <isitum01@gmail.com>
2024-02-14 17:32:38 +0100

    Handling API Error response (Delete Activity)

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -40,8 +40,6 @@ public class ActivitiesController : BaseApiController
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteActivity(Guid id)
     {
-        await Mediator.Send(new Delete.Command { Id = id });
-
-        return Ok();
+        return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
     }
 }
